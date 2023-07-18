ae88c9a1050f961e6fdbf894252e3ed5a7980f5d
xyzeez <106898953+xyzeez@users.noreply.github.com>
2023-07-18 03:09:26 +0100

    Added functionalities and UI

--- script.js
+++ script.js
@@ -1,63 +1,85 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    let playerScore = 0;
-    let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
+const playerScoreDisplay = document.querySelector('#player-result');
+const computerScoreDisplay = document.querySelector('#computer-result');
+let playerChoice;
+let computerChoice;
+const playerSelectionDisplay = document.querySelector('#player-choice');
+const computerSelectionDisplay = document.querySelector('#computer-choice');
+const commentDisplay = document.querySelector('#game-comment');
+let comment = '';
 
     game = () => {
 
-        const playerChoice = prompt('Your Choice: ').toLowerCase();
-        let computerChoice;
-        (
-            getComputerChoice = () => {
-                let random = Math.floor(Math.random() * 3) + 1;
-                switch (random) {
-                    case 1:
-                        computerChoice = 'rock';
-                        break;
-                    case 2:
-                        computerChoice = 'paper';
-                        break;
-                    case 3:
-                        computerChoice = 'scissors';
-                        break;
-                }
-            }
-        )();
+        document.querySelectorAll('button').forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                playerChoice = e.target.value;
+                playerSelectionDisplay.textContent = playerChoice;
+
+                (
+                    () => {
+                        let random = Math.floor(Math.random() * 3) + 1;
+                        switch (random) {
+                            case 1:
+                                computerChoice = 'rock';
+                                break;
+                            case 2:
+                                computerChoice = 'paper';
+                                break;
+                            case 3:
+                                computerChoice = 'scissors';
+                                break;
+                        }
+                        computerSelectionDisplay.textContent = computerChoice; 
+                    }
+                )();
+
+                playRound(playerChoice, computerChoice)
+
+            })
+        })
     
         playRound = (playerChoice, computerChoice) => {
 
             if (playerChoice === computerChoice) {
-                return "Its a tie!";
+                comment = "Its a tie!";
             } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-                playerScore++;
-                return "You Win! Rock beats Scissors";
+                //playerScore++;
+                comment = "You Win! Rock beats Scissors";
             } else if (playerChoice === 'paper' && computerChoice === 'rock') {
                 playerScore++;
-                return "You Win! Paper beats Rock";
+                comment = "You Win! Paper beats Rock";
             } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
                 playerScore++;
-                return "You Win! Scissors beats Paper";
+                comment = "You Win! Scissors beats Paper";
             } else {
                 computerScore++;
-                return 'Computer Wins!'
+                comment = 'Computer Wins!'
+            }
+
+            playerScoreDisplay.textContent = playerScore;
+            computerScoreDisplay.textContent = computerScore;
+            commentDisplay.textContent = comment;
+
+            if (playerScore === 5 || computerScore === 5) {
+                if (playerScore === 5) {
+                    comment = "Player Wins the Game!";
+                } else {
+                    comment = "Computer Wins the Game!";
+                }
+                commentDisplay.textContent = comment;
+                
+                setTimeout(() => {
+                    playerScore = computerScore = 0;
+                    playerScoreDisplay.textContent = playerScore;
+                    computerScoreDisplay.textContent = computerScore;
+                    playerSelectionDisplay.textContent = ' ';
+                    computerSelectionDisplay.textContent = ' ';
+                    commentDisplay.textContent = ' ';
+                }, 1000)
             }
             
         }
     
-        console.log(playRound(playerChoice, computerChoice));
     }
-
-    for (let i = 0; i < 5; i++) {
-        game();
-    }
-    console.log("Player: " + playerScore, "Computer: " + computerScore);
-    const score = (playerScore > computerScore) ? 'Player Wins!' : 'Computer Wins!';
-    console.log(score);
-
-
-
-
-
-
-
-});
+    game();
